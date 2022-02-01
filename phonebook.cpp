8ecdd35b5b68d3caeba46f37b8ad399eecaf1872
Pavan R GHate <chemoALPHA@yandex.com>
2022-02-01 16:15:48 +0530

    2:FIST:2II:COMPLETE

--- phonebook.cpp
+++ phonebook.cpp
@@ -5,7 +5,7 @@ using namespace std;
  class PhoneBook
 {
 	public :
-		int number;
+	    long number;
 		string name;
 		string email;
 		
@@ -36,39 +36,166 @@ int main()
 		cout<<"---------------------------------------------------------------"<<endl;
 	cout<<"enter your choice :";
 	cin>>x;
+	int cntr=0;
 	while(x!=5)
 	{
+		
 		switch(x)
 		{
 			case 0:
+	    	{
+	    		int i;
+	    		{
+					if(cntr==0)
+	    		{
+	    			cout<<"your phonebook is empty please try adding some  into it\n";
+					break;				}
+					
+				cout<<"Here are your "<<cntr<<" contacts :"<<endl;
+				cout<<"Position  Name         Email          Number\n";
+			cout<<"---------------------------------------------------------------"<<endl;	
+				for(i=0;i<cntr;i++)
 				{
-				int i;
-			for(i=0;i<30;i++)	
-			{
-				cout<<p1[i].number<<"\t"<<p1[i].name<<"\t"<<p1[i].email<<"\n";
-				
-			}
+					cout<<i+1<<"\t"<<p1[i].name<<"\t "<<p1[i].email<<"\t"<<p1[i].number<<"\n";
+		     	}	
 					
-					
-					
-					
-					break;
+			cout<<"---------------------------------------------------------------"<<endl;	
+			break;
 					
 				}
 		    case 1:
 		    	{
+		    		string str1;
+		    		string mail;
+		    		string temp;
+		    		
+		    		getline(cin,temp);
+		    		cout<<"enter Name:";
+		    		getline(cin,str1);
+		    		cout<<"enter email:";
+		    		getline(cin,mail);
+		    		cout<<"enter Number:";
+		    	    long no;
+					cin>>no; 
+		    		cout<<endl;
+		    		
+		    		p1[cntr].number=no;
+		    		p1[cntr].name=str1;
+		    		p1[cntr].email=mail;
+		    		cntr++;
+		    		
+		    	
 		    		break;
 				}
 			case 2:
+			
+			
+			
+					if(cntr==0)
+	    		{
+	    			cout<<"your phonebook is empty please try adding some  into it\n";
+					break;			
+				}
+					
+				cout<<"Here are your "<<cntr<<" contacts :"<<endl;
+				cout<<"Position  Name         Email          Number\n";
+			cout<<"---------------------------------------------------------------"<<endl;	
+				for(int i=0;i<cntr;i++)
 				{
+					cout<<i+1<<"\t"<<p1[i].name<<"\t "<<p1[i].email<<"\t"<<p1[i].number<<"\n";
+		     	}	
+					
+			cout<<"---------------------------------------------------------------"<<endl;	
+			
+			cout<<"Enter postion of contact you want to remove :";
+			int pos;
+			if(pos>=cntr)
+			{
+				cout<<"Please Enter valid postion";
+				break;
+			}
+			cin>>pos;
+			pos--;
+			PhoneBook p2;
+			
+			for(int j=pos;j<cntr;j++)
+			{
+				p2=p1[j];
+				p1[j]=p1[j+1];
+				p1[j+1]=p2;
+			}
+			cntr--;
+			
+			cout<<"Removed Successfully !\n";
+			
+					
+					
+					
+					
 					break;
 				}
 			case 3:
 				{
+					
+				cout<<"Please postion of contact that you want to edit :";
+				int pos;
+				cin>>pos;
+				pos--;
+				if(pos>cntr)
+				{
+					cout<<"please enter valid postion";
+					break;
+				}
+				
+				cout<<"1:edit Name\t2:edit email\t3:editNumber\n";
+				cout<<"Enter your option :";
+				int ch;
+				cin>>ch;
+				
+				switch(ch)
+				{
+					case 1:
+						{
+						cout<<"enter the New Name :";
+						string temp;
+						string blank;
+						getline(cin,blank);
+						getline(cin,temp);
+						p1[pos].name=temp;
+					    cout<<"Updated Sucessfully\n";
+							break;
+						}
+						case 2:
+						{
+						cout<<"enter the New Email :";
+						string temp;
+						string blank;
+						getline(cin,blank);
+						getline(cin,temp);
+						p1[pos].email=temp;
+							cout<<"Updated Sucessfully\n";
+							break;
+						}
+						case 3:
+						{
+						cout<<"enter the New Number:";
+						long temp;
+						cin>>temp;
+						p1[pos].number=temp;
+							cout<<"Updated Sucessfully\n";
+							break;
+						}
+					
+				}
+				
 					break;
 				}
 			case 4:
 				{
+					
+					cout<<"You've Total of "<<cntr<<"  Contacts\n";
+					
+					
 					break;
 				}
 			case 5:
